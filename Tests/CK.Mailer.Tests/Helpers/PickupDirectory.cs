f6403ed786528f5bdd74dd6608ab4d2ea545c9e3
Romain Delorme-Glorieux <romain.delorme-glorieux@signature.one>
2024-10-16 19:08:55 +0200

    Remove CK.Mailer.AspNet, CK.Mailer.YodiiScript CK.Mailer.Core.Web.Sample projects and mailer service

--- Tests/CK.Mailer.Tests/Helpers/PickupDirectory.cs
+++ Tests/CK.Mailer.Tests/Helpers/PickupDirectory.cs
@@ -2,23 +2,22 @@ using MimeKit;
 using System.IO;
 using System.Linq;
 
-namespace CK.Mailer.Tests
-{
-    public class PickupDirectory
-    {
-        public static MimeMessage GetTheLastSentEmail( MailKitOptions options )
-        {
-            if( !Directory.Exists( options.PickupDirectoryPath ) ) return null;
+namespace CK.Mailer.Tests;
 
-            DirectoryInfo dir = new DirectoryInfo( options.PickupDirectoryPath );
-            FileInfo lastFile = dir.GetFiles()
-                .OrderByDescending( f => f.CreationTimeUtc )
-                .First();
-            
-            using( var stream = lastFile.OpenRead() )
-            {
-                return MimeMessage.Load( stream );
-            }
-        }
+public static class PickupDirectory
+{
+    public static MimeMessage? GetTheLastSentEmail( string pickupDirectory )
+    {
+        if( !Directory.Exists( pickupDirectory ) ) return null;
+
+        var lastFile = new DirectoryInfo( pickupDirectory )
+            .GetFiles()
+            .OrderByDescending( f => f.CreationTimeUtc )
+            .FirstOrDefault();
+
+        if( lastFile is null ) return null;
+
+        using var stream = lastFile.OpenRead();
+        return MimeMessage.Load( stream );
     }
 }
